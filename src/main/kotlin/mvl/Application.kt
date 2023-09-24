601000759b116f363a3e64f949636ea9152859ca
n3ziniuka5 <laurynas.tretjakovas@gmail.com>
2023-09-24 13:12:51 +0300

    initial commit

---
+++ src/main/kotlin/mvl/Application.kt
@@ -0,0 +1,16 @@
+package mvl
+
+import io.ktor.server.application.*
+import mvl.plugins.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.cio.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureDatabases()
+    configureMonitoring()
+    configureHTTP()
+    configureRouting()
+}
